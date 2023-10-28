ca2ccc3d8144ce2c6e979edcaf73cff83af3fd21
bennett <w2500513053@outlook.com>
2023-10-28 18:27:03 +0800

    修改返回的CDK，支持返回多个直播的CDK。

--- achieves/cdk.ts
+++ achieves/cdk.ts
@@ -1,20 +1,36 @@
 import { defineDirective, InputParameter } from "@/modules/command";
 import { get_cdk } from "#/mihoyo-cdk/util/api";
-import { ForwardElem } from "@/modules/lib";
+import { ForwardElem, ForwardElemCustomNode } from "@/modules/lib";
 
 export default defineDirective( "order", async ( { sendMessage, client }: InputParameter ) => {
-	const codes = await get_cdk();
-	if ( codes.length === 0 ) {
+	const cdks = await get_cdk();
+	if ( cdks.length === 0 ) {
 		await sendMessage( "暂无直播兑换码" );
 		return;
 	}
 	
 	const info = await client.getLoginInfo();
-	const nodes = codes.map( code => ( {
-		uin: client.uin,
-		name: info.data.nickname,
-		content: code
-	} ) );
+	
+	const nodes: ForwardElemCustomNode[] = [];
+	for ( let { title, codes } of cdks ) {
+		let tips: string = "";
+		if ( codes.length >= 3 ) {
+			tips = `${ title }-直播兑换码，兑换码存在有效期，请尽快兑换!`;
+		} else {
+			tips = `${ title }-直播兑换码，暂时仅获取到${ codes.length }个直播兑换码，请稍后再次获取`;
+		}
+		const item = codes.map( code => ( {
+			uin: client.uin,
+			name: info.data.nickname,
+			content: code
+		} ) )
+		nodes.push( {
+			uin: client.uin,
+			name: info.data.nickname,
+			content: tips
+		} )
+		nodes.push( ...item );
+	}
 	const forwardMsg: ForwardElem = {
 		type: "forward",
 		messages: nodes
---
+++ util/types.ts
@@ -0,0 +1,4 @@
+export interface CodeType {
+	title: string;
+	codes: string[];
+}
