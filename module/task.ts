f2f6ea116ca363d2e8a9031a935df0bf26fba822
bennett <bennett_asia@outlook.com>
2025-10-01 00:39:25 +0800

    chore: add debug log.
    Signed-off-by: bennett <bennett_asia@outlook.com>

--- module/task.ts
+++ module/task.ts
@@ -111,7 +111,9 @@ export class Task {
 			}
 			
 			// 把通知缓存入库
+			Bot.logger.info( `[miHoYo-cdk-task] var notifications values: `, this.notifications );
 			await Bot.redis.setHash( db_key.notificationStatus, this.notifications );
+			Bot.logger.info( `[miHoYo-cdk-task] var notify_ids values: `, this.notify_ids );
 			await Bot.redis.setHash( db_key.notify_ids, this.notify_ids );
 			await Bot.redis.setTimeout( db_key.notify_ids, this.EXPIRE_TIME );
 		} )
